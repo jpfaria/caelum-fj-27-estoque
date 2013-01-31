d8a86127e2e2e0453e726e8600309bb91c3eb131
jpfaria <jpfaria@gmail.com>
2013-01-31 21:50:39 -0200

    .

--- src/br/com/caelum/estoque/aop/LoggingAspect.java
+++ src/br/com/caelum/estoque/aop/LoggingAspect.java
@@ -1,10 +1,31 @@
 package br.com.caelum.estoque.aop;
 
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
+	
+	//Log4j
+	private Logger logger = Logger.getLogger(LoggingAspect.class);
+	
+	@Before("metodosDeServico()")
+	public void logaNoComeco(JoinPoint joinPoint) {
+		
+		String methodName = joinPoint.getSignature().getName();
+		String typeName = joinPoint.getSignature().getDeclaringTypeName();
+		
+		logger.info("Executando metodo: " + methodName + " da classe: " + typeName);
+		
+	}
+	
+	
+	@Pointcut("execution(* br.com.caelum.estoque.service..*.*(..))")
+	public void metodosDeServico() {}
 
 }
