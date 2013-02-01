0b7600ab6127a43cfcaca279c210f2924c90e742
jpfaria <jpfaria@gmail.com>
2013-02-01 18:44:31 -0200

    .

--- src/br/com/caelum/estoque/aop/LoggingAspect.java
+++ src/br/com/caelum/estoque/aop/LoggingAspect.java
@@ -1,13 +1,9 @@
 package br.com.caelum.estoque.aop;
 
 import org.apache.log4j.Logger;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -17,6 +13,7 @@ public class LoggingAspect {
 	//Log4j
 	private Logger logger = Logger.getLogger(LoggingAspect.class);
 	
+	/*
 	@Before("metodosDeServico()")
 	public void logaNoComeco(JoinPoint joinPoint) {
 		
@@ -29,19 +26,26 @@ public class LoggingAspect {
 	
 	@Pointcut("execution(* br.com.caelum.estoque.service..*.*(..))")
 	public void metodosDeServico() {}
-	
+	*/
 	
 	@Around("execution(* br.com.caelum.estoque.dao..*.*(..))")
-	public void metodosDeDao(ProceedingJoinPoint joinPoint) throws Throwable {
+	public Object metodosDeDao(ProceedingJoinPoint joinPoint) {
 		
 		String methodName = joinPoint.getSignature().getName();
 		String typeName = joinPoint.getSignature().getDeclaringTypeName();
-		
 		logger.info("Executando antes do metodo: " + methodName + " da classe: " + typeName);
 		
-		joinPoint.proceed();
+		Object obj = null;
+		
+		try {
+			obj = joinPoint.proceed();
+		} catch (Throwable e) {
+			logger.info("Exception: " + e.getMessage());
+		}
 		
 		logger.info("Executando depois do metodo: " + methodName + " da classe: " + typeName);
+		
+		return obj;
 		
 	}
 
